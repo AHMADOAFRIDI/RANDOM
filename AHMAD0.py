2128e83aca6c14efa7bea7212daa5f5f0598ee1e
AHMAD ALI AFRIDI <89185243+AHMADOAFRIDI@users.noreply.github.com>
2023-02-21 01:06:02 +0500

    Create AHMAD0.py

---
+++ AHMAD0.py
@@ -0,0 +1,8 @@
+import os, platform
+os.system('git pull')
+import requests
+os.system("xdg-open https://www.facebook.com/profile.php?id=100009482583225")
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from AHMAD0 import fia
+    fia()
