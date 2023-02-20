14571f31cb270ec2e746e374da0923f47837ed2d
AHMAD ALI AFRIDI <89185243+AHMADOAFRIDI@users.noreply.github.com>
2023-02-21 01:21:30 +0500

    Delete AHMAD0.py

--- AHMAD0.py
+++
@@ -1,8 +0,0 @@
-import os, platform
-os.system('git pull')
-import requests
-os.system("xdg-open https://www.facebook.com/profile.php?id=100009482583225")
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from AHMAD0 import fia
-    fia()
